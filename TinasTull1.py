33e17990283a86c121f966761be2fadc8285522b
JonasKrSunde <jonas.k.sunde@ntnu.no>
2016-10-22 16:46:34 +0200

    commenting and titling

--- TinasTull1.py
+++ TinasTull1.py
@@ -31,31 +31,27 @@ NA = 6.022*10**23 # Avogadro's constant, [particles/mol]
 "Isothermal annealing at T = 400 [C]"
 T_K = 273.15 # Deg K at 0 deg C
 T_i = 400.0+T_K # [K]
-T_low = 400+T_K
-T_hi = 430+T_K
+T_low = 400.0+T_K # [K]
+T_hi = 430.0+T_K # [K]
 
 "From table 1 in Bjørneklett"
-C_star=2.17e1 # wt%/100
+C_star=2.17e1 # wt.%/100
 DeltaH=50.8e3 # [J/mol]
 D_0 = 3.46e7 # [um^2*s^-1]
 Q = 123.8e3 # [J/mol]
 B_0=1e-3 # [um]
 r_0=0.025 # [um]
-C_p=1.0 # [at%]
-C_0=0.0  # [at]      
-
-
+C_p=1.0 # [wt.%]
+C_0=0.0  # [wt.%]      
 
 #Diffusivity for T_i
 D_i = D_0*np.exp(-Q/(R*T_i))
 
-
-
-"Spacial and temporal discretisation"
-N = 300 # Number of spacial partitions of bar
-L = 1.5 # [um] Length of barH = 30.0 
-#t_i = 0.1 # senconds for isothermal annealing
-t_i = 20 # senconds for isothermal annealing
+"Spatial and temporal discretisation"
+N = 300 # Number of spatial partitions of bar
+L = 1.5 # [um] Length of bar 
+#t_i = 0.1 # seconds for isothermal annealing
+t_i = 15 # seconds for isothermal annealing
 #T1 = 1e3+T_K # [K] Temperature           
 x_bar = np.linspace(0,L,N+1)
 dx = L/N   # Need N+1 x points, where [N/2] is centered in a 0-indexed array
@@ -64,24 +60,31 @@ alpha = .4  # alpha = D*dt/dx**2 --> Const in discretisation --> Must be <= 0.5 
 
 
 "Precipitation of pure Si particles in a binary Al-Si alloy, assuming a diluted Al matrix."
-# Calculating and plotting concentration profile for the spatial range [-1,+1] mm after 20 s annealing at 400 deg C
+# Calculating and plotting concentration profile for the spatial range [0,1.5] um after 20 s annealing at 400 deg C
 
+# Concentration at interface at temperature Ttemp
 def C_i_f(Ttemp):
-    return C_star*np.exp(-DeltaH/(R*Ttemp))
+    C = C_star*np.exp(-DeltaH/(R*Ttemp))
+    #print('Concentration at interface at temperature %.1f K: %e mol/um' % (Ttemp,C))
+    return C
 
-# Calculates diffusivity
+# Calculates diffusivity at temp. T
 def Diffusivity(T):
     return D_0*np.exp(-Q/(R*T))
 
+# Calculates concentration coefficient k_f
 def k_f(C_it):
     return 2*(C_it-C_0)/(C_p-C_0)
+    #return 2*(C_0-C_it)/(C_p-C_it)
     
 #def Bf(k,t,D,B_init,t_init):
 #   return (B_init-k*(np.sqrt(D*t/pi)-np.sqrt(D*t_init/pi)))/B_0
 
 def Bf(k,t,D,B_init):
     return (B_init-k*(np.sqrt(D*t/pi)))/B_0
-print(pi/D_i*B_0**2/k_f(C_i_f(T_i))**2)
+    
+print('Total time req. for complete particle dissol.: %f s' % (pi/D_i*B_0**2/k_f(C_i_f(T_i))**2))
+
 #Calculate the concentration on the particle surface at the temperature T_i
 def Csurf(T):
     return C_star*np.exp(-DeltaH/(R*T))
@@ -100,13 +103,13 @@ def CAnal(x,r,T,D,t):
 def AnalConc():
     Conc = [CAnal(i,r_0,T_i,D_i,t_i) for i in x_bar]
     plt.plot(x_bar,Conc,',') #label='Si'
-    plt.xlim(0, L)
+    plt.xlim(0, L+.1)
     plt.ylim(0, 1.1)
     plt.xlabel('x [um]')
     plt.ylabel('Concentration [mol/um]')
     plt.title('Analytic concentration profile of Si  after %d seconds annealing at %d K' % (t_i, T_i))
     plt.legend(bbox_to_anchor=(0.2,1))
-    plt.rcParams.update({'font.size': 18})
+    plt.rcParams.update({'font.size': 16})
     return Conc
        
 
@@ -141,9 +144,6 @@ def NextB(D_temp, k_temp, t_temp, dt_temp, B_prev):
     if B_temp < 0:
         return 0
     return B_temp
-
-
-   
 
 def finite_diff():
     # Spatial discretisation is global
@@ -261,11 +261,11 @@ def stabilityCheck(exact,approx):
     plt.rcParams.update({'font.size': 16})
 
 def main(argv):
- #   analytical = AnalConc() # Calc and plot concentration profiles, analytical formula
-#    finite_diff() # Calc and plot concentration profiles, finite differences
+    analytical = AnalConc() # Calc and plot concentration profiles, analytical formula
+    finite_diff() # Calc and plot concentration profiles, finite differences
     #Plate_thickness()    
-    fin_diff_two_step(T_hi,T_low)
-    plt.show()
+    #fin_diff_two_step(T_hi,T_low)
+    #plt.show()
 #    fin_diff_wLin_Temp_profile_Cu() # Calc and plot concentration profile for Cu, linear temp. increase
     
 #    stabilityCheck(analytical,fin_diff) # Comparison analytical and finite differences
